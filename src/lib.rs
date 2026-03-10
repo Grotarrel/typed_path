d6e7f3261c9c5a7f61ca69424e304ccde9d24570
grotarrel <g@rrel.fr>
2026-03-10 13:55:07 +0100

    main types defined: ExistingX and NewX with X = Path, File or Dir

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::PathBuf;
+
+
+/// Type representing a path that actually exists in the filesystem
+/// (at the moment of creation of the ExistingPath object).
+struct ExistingPath(PathBuf);
+
+/// Type representing a file path that actually exists in the filesystem
+/// (at the moment of creation of the ExistingFile object).
+struct ExistingFile(PathBuf);
+
+/// Type representing a directory path that actually exists in the filesystem
+/// (at the moment of creation of the ExistingDir object).
+struct ExistingDir(PathBuf);
+
+/// Type representing a path that might not yet exist in the filesystem
+/// but is consistent (at the moment of creation of the NewPath object),
+/// meaning that no parent is an existing file (all parents are either existing directories,
+/// or do not exist yet).
+struct NewPath {
+    path: PathBuf,
+    root: PathBuf,
 }
+
+/// Type representing a file path that might not yet exist in the filesystem
+/// but is consistent (at the moment of creation of the NewFile object),
+/// meaning that no parent is an existing file (all parents are either existing directories,
+/// or do not exist yet).
+struct NewFile {
+    path: PathBuf,
+    root: PathBuf,
+}
+
+/// Type representing a directory path that might not yet exist in the filesystem
+/// but is consistent (at the moment of creation of the NewDir object),
+/// meaning that no parent is an existing file (all parents are either existing directories,
+/// or do not exist yet).
+struct NewDir {
+    path: PathBuf,
+    root: PathBuf,
+}
+
+
+
+
+
+
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
 }
